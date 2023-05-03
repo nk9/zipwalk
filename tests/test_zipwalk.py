13ac59ac831b4e9a8c351f381f38463f4af77ff2
Joao Meyer <1994meyer@gmail.com>
2023-05-03 21:17:31 +0100

    Working version

---
+++ tests/test_zipwalk.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+from zipwalk import zipwalk
+
+
+ZIPFILE = 'tests/1.zip'
+
+
+class TestZipWalk(TestCase):
+    def test_zipwalk(self):
+        """ Iterates over the zipfile """
+        walker = zipwalk(ZIPFILE)
+
+        # first level
+        root, zips, files = next(walker)
+        self.assertEqual(root.filename, 'tests/1.zip')
+        self.assertSetEqual(zips, {'2.zip'})
+        self.assertSetEqual(files, {'1a.txt', '1b.txt', '1c.txt', 'dir/d1.txt'})
+
+        # second level
+        root, zips, files = next(walker)
+        self.assertEqual(root.filename, '2.zip')
+        self.assertSetEqual(zips, set())
+        self.assertSetEqual(files, {'2a.txt', '2b.txt', '2c.txt'})
+
+    def test_total_files(self):
+        """ Counts files """
+        allfiles = set()
+
+        for _, _, files in zipwalk(ZIPFILE):
+            allfiles |= files
+        
+        self.assertEqual(len(allfiles), 7)
